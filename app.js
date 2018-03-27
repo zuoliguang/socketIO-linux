57d1d019e9b0bf469286ce2bb415f6a2be3a919d
zuoliguang <zlgcg@sina.com>
2018-03-27 16:05:30 +0800

    广播调试

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ var server = http.createServer(function (req,res){
         res.end(data,'utf-8');
     });
 }).listen(8000,"192.168.147.131");
+
 console.log('Server running at http://192.168.147.131:8000/');
 
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket){
  
-// 向客户端发送信息
+// 向客户端 test_news 频道 发送信息
     socket.emit('test_news', { hello:'world' });
 
-// 接收客户端信息并广播出去
+// 接收客户端广播信息 并向 broadnews 广播频道广播出去
     socket.on('broadcast', function(data){
         socket.emit('broadnews', data);
         socket.broadcast.emit('broadnews', data);
